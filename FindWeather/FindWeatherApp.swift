c6879958b279ec00cd9b13de003e0823ea0fee1d
vinh <dr.vinhhoang@gmail.com>
2021-12-15 15:13:27 +0700

    Initial Commit

---
+++ FindWeather/FindWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindWeatherApp.swift
+//  FindWeather
+//
+//  Created by VINH HOANG on 15/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct FindWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
